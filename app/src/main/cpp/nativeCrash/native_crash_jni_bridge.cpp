b92718176c42dec76bf44933e09e35a93b484d15
wangfayou <coder.wangfayou@gmail.com>
2025-04-03 17:38:50 +0800

    update

--- app/src/main/cpp/nativeCrash/native_crash_jni_bridge.cpp
+++ app/src/main/cpp/nativeCrash/native_crash_jni_bridge.cpp
@@ -3,12 +3,9 @@
 #include "native_crash_handler.h"
 
 JavaVM *_vm;
-
-
 //需要动态注册native方法的 Java类名   当前native_crash_jni_bridge.cpp是所有JNI的代理类
 static const char *className = "com/github/crash/crash/cnative/NativeCrash";
 
-// ---------动态注册-----------
 extern "C"
 JNIEXPORT void JNICALL
 testCrash(JNIEnv *env, jclass clazz) {
@@ -16,8 +13,56 @@ testCrash(JNIEnv *env, jclass clazz) {
     abort();
 }
 
+extern "C"
+JNIEXPORT void JNICALL
+InitCrashHandler(JNIEnv *env,
+                 jclass clazz,
+                 jstring log_dir,
+                 jstring version,
+                 jobject callback) {
+    const char *path = env->GetStringUTFChars(log_dir, nullptr);
+    const char *ver = env->GetStringUTFChars(version, nullptr);
+    CrashHandler::Init(env, path, callback);
+    CrashHandler::SetVersion(ver);
+    env->ReleaseStringUTFChars(log_dir, path);
+    env->ReleaseStringUTFChars(version, ver);
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+SetVersion(JNIEnv *env,
+           jclass clazz,
+           jstring version) {
+    CrashHandler::SetVersion(env->GetStringUTFChars(version, nullptr));
+}
+extern "C"
+JNIEXPORT jint JNICALL
+DeleteCrashLogFile(JNIEnv *env, jclass clazz,
+                   jstring log_path) {
+    const char *path = env->GetStringUTFChars(log_path, nullptr);
+    int result = CrashHandler::deleteLogFile(path);
+    env->ReleaseStringUTFChars(log_path, path);
+    return result;
+}
+
 //需要动态注册的native方法数组
-static const JNINativeMethod methods[] = {{"testCrash", "()V", (void *) testCrash}};
+static const JNINativeMethod methods[] = {{"testCrash", "()V", (void *) testCrash},
+                                          {"initCrashHandler",
+                                                        "(Ljava/lang/String;Ljava/lang/String;Lcom/github/crash/crash/cnative/NativeCrashCallback;)V",
+                                                               (void *) InitCrashHandler},
+                                          {
+                                           "SetVersion",
+                                                        "(Ljava/lang/String;)V",
+                                                               (void *) SetVersion
+                                          },
+                                          {
+                                           "deleteCrashLogFile",
+                                                        "(Ljava/lang/String;)I",
+                                                               (void *) DeleteCrashLogFile
+                                          }
+
+};
 
 
 //调用System.loadLibrary()函数时， 内部就会去查找so中的 JNI_OnLoad 函数，如果存在此函数则调用。
@@ -47,43 +92,4 @@ void JNI_OnUnload(JavaVM *vm, void *reserved) {
         jclass registerClass = env->FindClass(className);
         env->UnregisterNatives(registerClass);
     }
-}
-
-
-//在jni中使用的Java_com_hellondk_NDKTool_test来进行与java方法的匹配，这种方式我们称之为静态注册。
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_github_crash_crash_cnative_NativeCrash_initCrashHandler(JNIEnv *env,
-                                                                 jclass clazz,
-                                                                 jstring log_dir,
-                                                                 jstring version,
-                                                                 jobject callback) {
-
-
-    const char *path = env->GetStringUTFChars(log_dir, nullptr);
-    const char *ver = env->GetStringUTFChars(version, nullptr);
-    CrashHandler::Init(env, path, callback);
-    CrashHandler::SetVersion(ver);
-    env->ReleaseStringUTFChars(log_dir, path);
-    env->ReleaseStringUTFChars(version, ver);
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_github_crash_crash_cnative_NativeCrash_SetVersion(JNIEnv *env, jclass clazz,
-                                                           jstring version) {
-    CrashHandler::SetVersion(env->GetStringUTFChars(version, nullptr));
-}
-
-
-
-
-extern "C"
-JNIEXPORT jint JNICALL
-Java_com_github_crash_crash_cnative_NativeCrash_deleteCrashLogFile(JNIEnv *env, jclass clazz,
-                                                                   jstring log_path) {
-    const char *path = env->GetStringUTFChars(log_path, nullptr);
-    int result = CrashHandler::deleteLogFile(path);
-    env->ReleaseStringUTFChars(log_path, path);
-    return result;
 }
